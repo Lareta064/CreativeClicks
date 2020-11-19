9720c40e52aa598a3f79cfce0c2ca20b7ae2bb92
Lareta <lareta064@gmail.com>
2020-11-19 11:15:25 +0300

    mob-menu

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 	// SHOW MOB MENU
 	const bodyEl = document.body;
 	const menuToggle = document.querySelector('.menu-toggle');
-	// const mobMenu = document.querySelector('#mobile-menu');
+	const mobMenu = document.querySelector('.header-navigation');
 	// const overlayBlock = document.querySelector('#overlay');
 	// const mobHeader = document.querySelector('.header');
 	
@@ -11,36 +11,36 @@ $(document).ready(function () {
 		menuToggle.addEventListener('click', function () {
 			if (this.classList.contains('active')) {
 				this.classList.remove('active');
-				// mobMenu.classList.remove('active');
+				mobMenu.classList.remove('active');
 				// overlayBlock.classList.remove('active');
-				// bodyEl.classList.remove('noscroll');
+				bodyEl.classList.remove('noscroll');
 				// mobHeader.classList.remove('active');
 
 			} else {
 				this.classList.add('active');
-				// mobMenu.classList.add('active');
+				mobMenu.classList.add('active');
 				// overlayBlock.classList.add('active');
 				// mobHeader.classList.add('active');
-				// bodyEl.classList.add('noscroll');
+				bodyEl.classList.add('noscroll');
 
 			}
 		});
 		window.addEventListener('resize', function () {
 			menuToggle.classList.remove('active');
 			// overlayBlock.classList.remove('active');
-			// bodyEl.classList.remove('noscroll');
-			// mobMenu.classList.remove('active');
+			bodyEl.classList.remove('noscroll');
+			mobMenu.classList.remove('active');
 			// mobHeader.classList.remove('active');
 
 		});
-		// mobMenu.addEventListener('click', function () {
-		// 	this.classList.remove('active');
-		// 	// menuToggle.classList.remove('active');
+		mobMenu.addEventListener('click', function () {
+			this.classList.remove('active');
+			 menuToggle.classList.remove('active');
 		// 	// overlayBlock.classList.remove('active');
 		// 	// mobHeader.classList.remove('active');
-		// 	// bodyEl.classList.remove('noscroll');
+		 bodyEl.classList.remove('noscroll');
 
-		// })
+		})
 	}
    let caseSlider = $('.case-slider');
    let caseSliderArrowLeft = $('button.arr-left');

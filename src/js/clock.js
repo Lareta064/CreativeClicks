3be8dec89556b8bda87499af88b7ecc3701eb4b0
Lareta <lareta064@gmail.com>
2020-12-14 11:20:09 +0300

    form desk

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,52 +1,53 @@
-//Seconds value on the page load
-let initSeconds = moment().seconds();
+let haveClock = document.getElementById('offices')
+if (haveClock){
+    //Seconds value on the page load
+    let initSeconds = moment().seconds();
 
-//Miliseconds until the timeout start
-let synch = (60 - initSeconds) * 1000;
+    //Miliseconds until the timeout start
+    let synch = (60 - initSeconds) * 1000;
 
-//Different time zones UTC offsets
-let zonesOffsets = [1, 8, 2, -5, -8];
+    //Different time zones UTC offsets
+    let zonesOffsets = [1, 8, 2, -5, -8];
 
-//Initial activation
-updateClock(zonesOffsets);
+    //Initial activation
+    updateClock(zonesOffsets);
 
-//Function that sets the time on the clock and check for working hours
-function updateClock(x)
-{
-    setClock(x);
-    dimOutClock(x.length, 8, 20);
-}
-
-//Function that sets the values to clocks
-function setClock(x)
-{
-    for (let i = 0; i < x.length; i++)
+    //Function that sets the time on the clock and check for working hours
+    function updateClock(x)
     {
-        let y = document.getElementById("clock" + (i + 1));
-        y.innerHTML = moment().utcOffset(x[i]).format("HH:mm");
+        setClock(x);
+        dimOutClock(x.length, 8, 20);
     }
-}
 
-//Function that checks the local time for "x" clock divs, change styles if not in interval [t1 - t2)
-function dimOutClock(x, t1, t2)
-{
-    for (i = 0; i < x; i++)
+    function setClock(x)
     {
-        let y = document.getElementById("clock" + (i + 1));
-        let z = parseInt(y.innerHTML);
-        if ((z < t1 ) || (z >= t2))
+        for (let i = 0; i < x.length; i++)
         {
-            y.parentElement.classList.add('offices-group__item-disable');
-        } else
-        {
-            y.parentElement.classList.remove('offices-group__item-disable');
+            let y = document.getElementById("clock" + (i + 1));
+            y.innerHTML = moment().utcOffset(x[i]).format("HH:mm");
         }
     }
+
+    //Function that checks the local time for "x" clock divs, change styles if not in interval [t1 - t2)
+    function dimOutClock(x, t1, t2)
+    {
+        for (i = 0; i < x; i++)
+        {
+            let y = document.getElementById("clock" + (i + 1));
+            let z = parseInt(y.innerHTML);
+            if ((z < t1 ) || (z >= t2))
+            {
+                y.parentElement.classList.add('offices-group__item-disable');
+            } else
+            {
+                y.parentElement.classList.remove('offices-group__item-disable');
+            }
+        }
+    }
+
+    //Start clock
+    setTimeout(setInterval(() => {updateClock(zonesOffsets)}, 60000), synch);
 }
-
-//Start clock
-setTimeout(setInterval(() => {updateClock(zonesOffsets)}, 60000), synch);
-
 
 
 
--- src/js/main.js
+++ src/js/main.js
@@ -51,69 +51,268 @@ $(document).ready(function () {
 	caseSliderArrowRight.click(function () {
 		caseSlider.trigger("prev.owl.carousel");
 	});
-	
-	let partnersSlider = $('.partners-slider');
-	partnersSlider.owlCarousel({
-		items:1,
-		smartSpeed:800,
-		touchDrag : false
-
-	});
-	// FORM FAKE PLACEHOLDER
-	const formItem = document.querySelector('.form-item');
-	if(formItem){
-		let inputElement = formItem.querySelector('input');
-		let placeholderElement = formItem.querySelector('.fake-placeholder');
-		
-		inputElement.addEventListener('focus', function(){
-			placeholderElement.classList.add('active');
-		});
-		
-		inputElement.addEventListener('blur', function(){
-			if(inputElement.value == ''){
-				placeholderElement.classList.remove('active');
-			}
-			else{
-				placeholderElement.classList.add('active');
-			}
+	//SHOW/HIDE PASSWORD
+	let inputPassIcon = document.querySelectorAll('.icon-passw');
+	if(inputPassIcon){
+		for(let item of inputPassIcon){
+			item.addEventListener('click', function(){
+				const thisParent = this.closest('.form-item');
+				const inputItem = thisParent.querySelector('input');
+				const thisImgs = this.querySelectorAll('img');
+				for(let img of thisImgs){
+					img.classList.toggle('active');
+				}
+				let inputType = inputItem.getAttribute('type');
+				if(inputType == 'password'){
+					inputItem.setAttribute('type', 'text');
+				}
+				else{
+					inputItem.setAttribute('type', 'password');
+				}
+			});
+		}
+	}
+	//TOGGLE ARROW FOR SELECR COUNTRY
+	const selectCountry = document.querySelector('#select-country');
+	if(selectCountry){
+		const thisArrow = document.querySelector('.select-arr');
+		selectCountry.addEventListener('click', function(){
+			thisArrow.classList.toggle('rotate');
 			
 		});
 	}
-	// FORM VALIDATE
-	$('#contact-form').validate({
-        rules: {
-            email: {
-                required: true,
-                email: true
-            }
-        },
-        messages: {
-            email: {
-                required: 'field should not be empty',
-                email: 'missing @ symbol'
-            }
-            
-        },
-        submitHandler: function (form) {
-            ajaxFormSubmit();
-        }
-    });
+	//========================================================================
+	//========================================================================
+	//SCROLL LOGIC and ADDITIVES
+	//========================================================================
+	//========================================================================
+	window.addEventListener("scroll", onScrollEventHanlder);
 
-    //  AJAX function
-    function ajaxFormSubmit() {
+	
+	const SLIDER_NOT_REACHED = 0;
+	const SLIDER_AT_THE_BEGGINING = 1;
+	const SLIDER_IN_PROGRESS = 2;
+	const SLIDER_FINISHED = 3;
 
-        let string = $("#contact-form").serialize();
-        $.ajax({
-            type: "POST", 
-            url: "php/mail.php",
-            data: string, 
-           
-            success: function (html) {
-                $("#contact-form").slideUp(800);
-                $('#answer').html('success');
-            }
-        });
-        return false;
-    }
-   
+	var partnersSlider = {
+		state: SLIDER_NOT_REACHED,
+		translating: false,
+		currentIndex: 0,
+		anchorY: 0,
+		element: $(".partners-slider"),
+		showNext: 
+			function(visible){
+				if(!visible){
+					$(".partners-slider").nextAll().addClass("hideDOM");
+					$(".partners").nextAll().addClass("hideDOM");
+				}else{
+					$(".partners-slider").nextAll().removeClass("hideDOM");
+					$(".partners").nextAll().removeClass("hideDOM");
+				}				
+			}
+	}
+
+	
+	if($(".partners-slider").length>0){
+		console.log($(".partners-slider").length);
+		partnersSlider.element.owlCarousel({
+			items:1,
+			loop: false,
+			mouseDrag: false,
+			touchDrag: false,
+			navSpeed: 400,
+			smartSpeed:400,		
+			onTranslate: function(){
+				partnersSlider.translating = true;
+			},
+			onTranslated: onTranslatedEventHandler
+		 });
+	
+		partnersSlider.showNext(false);
+
+		window.addEventListener("wheel", onWheelEventHanlder, {passive:false});
+		window.addEventListener("touchstart", onTouchEventHandler, {passive:false});
+		window.addEventListener("touchmove", onTouchEventHandler, {passive:false});
+		window.addEventListener("touchend", onTouchEventHandler, {passive:false});
+		window.addEventListener("keydown", onKeydownEventHandler);
+		window.addEventListener("mousedown", onMouseEventHandler);
+	}
+	
+
+	let bars = document.querySelectorAll(".progress-bar");
+	for (let bar of bars) {
+		bar.style.width = "424px";
+	}
+
+	function onScrollEventHanlder(scrollEvent){
+		let lowerScreenBoundary = Math.ceil(window.innerHeight+window.scrollY);
+		if($(".partners-slider").length>0){			
+			if((partnersSlider.state == SLIDER_NOT_REACHED)&&(lowerScreenBoundary >= document.body.offsetHeight)){
+				partnersSlider.state = SLIDER_AT_THE_BEGGINING;			
+				partnersSlider.anchorY = window.scrollY;
+				// $("#debug").text("STATE: "+partnersSlider.state);	
+				// console.log("Stage 1: Slider at the beggining");
+			}else if((partnersSlider.state == SLIDER_AT_THE_BEGGINING)&&(lowerScreenBoundary < document.body.offsetHeight)){
+				partnersSlider.state = SLIDER_NOT_REACHED;
+				// $("#debug").text("STATE: "+partnersSlider.state);	
+				// console.log("Stage 0: Slider is out of focus");
+			}
+		}
+		// else if((partnersSlider.state == SLIDER_FINISHED)&&($(window).scrollTop() < partnersSlider.element.offset().top)){
+		// 	partnersSlider.state = SLIDER_NOT_REACHED;
+		// 	partnersSlider.showNext(false);
+		// }
+		for (let bar of bars) {
+			let maxWidth = bar.parentElement.offsetWidth;
+			// let computedWidth = 
+			// 	Math.max(0, 
+			// 		Math.min(
+			// 			maxWidth, 
+			// 			//bar.getBoundingClientRect().top>window.innerHeight/2 ? bar.getBoundingClientRect().top-window.innerHeight : window.innerHeight-bar.getBoundingClientRect().top
+			// 			Math.round(
+			// 					(maxWidth+424*2)-((window.innerHeight - bar.getBoundingClientRect().top)/window.innerHeight)*(maxWidth+424*2)
+			// 				)
+			// 			)
+			// 		);
+
+
+			let computedOffset = Math.round(-424 + ((window.innerHeight - bar.getBoundingClientRect().top)/window.innerHeight)*(maxWidth+424));
+			let computedWidth = computedOffset < 0 ? computedOffset+424 : maxWidth-computedOffset;
+			computedOffset = Math.max(0, Math.min(maxWidth, computedOffset));
+			computedWidth = Math.max(0, Math.min(424, computedWidth));
+			// let computedWidth = 
+			// 	Math.max(0, 
+			// 		Math.min(
+			// 			maxWidth, 
+			// 			//bar.getBoundingClientRect().top>window.innerHeight/2 ? bar.getBoundingClientRect().top-window.innerHeight : window.innerHeight-bar.getBoundingClientRect().top
+			// 			Math.round(
+			// 					(maxWidth+424*2)-((window.innerHeight - bar.getBoundingClientRect().top)/window.innerHeight)*(maxWidth+424*2)
+			// 				)
+			// 			)
+			// 		);
+			bar.style.width = computedWidth+"px";
+			if(bar.classList.contains("progress-bar--right"))
+				bar.style.right = computedOffset+"px";
+			else
+				bar.style.left = computedOffset+"px";
+		}
+	}
+
+	let lastTouches;
+
+	function onTouchEventHandler(touchEvent){
+		switch(touchEvent.type){
+			case "touchstart":
+				lastTouches = touchEvent.touches;    
+				
+			break;
+			case "touchmove":
+				if((partnersSlider.state == SLIDER_AT_THE_BEGGINING)){					
+					let dir = touchesToDirection(lastTouches, touchEvent.changedTouches);
+					if(dir>0){
+						touchEvent.preventDefault();
+						partnersSlider.state = SLIDER_IN_PROGRESS;
+						// console.log("Stage 2: Slider is in progress");
+						// $("#debug").text("STATE: "+partnersSlider.state);
+						partnersSlider.element.trigger("next.owl.carousel");
+					}
+				}else if((partnersSlider.state==SLIDER_IN_PROGRESS)){
+					touchEvent.preventDefault();
+					let dir = touchesToDirection(lastTouches, touchEvent.changedTouches);
+					if(partnersSlider.translating)
+						break;
+					if(dir>0){
+						partnersSlider.element.trigger("next.owl.carousel");
+					}else{
+						partnersSlider.element.trigger("prev.owl.carousel");
+					}
+				}
+			break;
+		}
+	}
+
+	function touchesToDirection(initialTouches, updatedTouches){
+		let direction = 0;
+		for(let i=0; i<updatedTouches.length; i++){
+			for(let j=0; j<initialTouches.length; j++)
+			if(updatedTouches[i].identifier==initialTouches[j].identifier){
+				if(updatedTouches[i].clientY>initialTouches[j].clientY){
+					direction--;
+				}else if ((updatedTouches[i].clientY<initialTouches[j].clientY)){
+					direction++;
+				}
+			}
+		}
+		return direction;
+	}
+
+	function onWheelEventHanlder(wheelEvent){
+		if(partnersSlider.state!=SLIDER_NOT_REACHED && partnersSlider.state!=SLIDER_FINISHED)
+			wheelEvent.preventDefault();
+		if(partnersSlider.state==SLIDER_AT_THE_BEGGINING){
+			wheelEvent.preventDefault();
+			if(wheelEvent.deltaY>0){
+				
+				partnersSlider.state = SLIDER_IN_PROGRESS;
+				// console.log("Stage 2: Slider is in progress");
+				// $("#debug").text("STATE: "+partnersSlider.state);
+				partnersSlider.element.trigger("next.owl.carousel");
+			}else{
+				window.scrollBy(0, -100);
+			}
+		}else if(partnersSlider.state==SLIDER_IN_PROGRESS){
+			wheelEvent.preventDefault();
+			if(!partnersSlider.translating)
+				if(wheelEvent.deltaY>0){
+					partnersSlider.element.trigger("next.owl.carousel");
+				}else{
+					partnersSlider.element.trigger("prev.owl.carousel");
+				}
+		}
+	}
+
+	function onTranslatedEventHandler(owlEvent){
+		partnersSlider.translating = false;
+		partnersSlider.currentIndex = owlEvent.item.index;
+		if(partnersSlider.currentIndex == 0){
+			partnersSlider.state = SLIDER_AT_THE_BEGGINING;
+			// console.log("Stage 1: Slider at the beggining");
+			// $("#debug").text("STATE: "+partnersSlider.state);
+		}else if(partnersSlider.currentIndex == owlEvent.item.count-1){
+			partnersSlider.state = SLIDER_FINISHED;
+			// console.log("Stage 3: Slider completed");
+			// $("#debug").text("STATE: "+partnersSlider.state);
+			partnersSlider.showNext(true);
+						
+			partnersSlider.element.trigger('destroy.owl.carousel');
+			partnersSlider.element.owlCarousel({
+				items:1,
+				startPosition: 11,
+				loop: true,
+				autoplay: true,
+				mouseDrag: true,
+				touchDrag: true,
+				navSpeed: 400,
+				smartSpeed:400,
+			 });
+
+			window.removeEventListener("keydown", onKeydownEventHandler);
+			window.removeEventListener("mousedown", onMouseEventHandler);
+			window.removeEventListener("wheel", onWheelEventHanlder, {passive:false});
+			window.removeEventListener("touchstart", onTouchEventHandler, {passive:false});
+			window.removeEventListener("touchmove", onTouchEventHandler, {passive:false});
+			window.removeEventListener("touchend", onTouchEventHandler, {passive:false});
+		}
+	}	
+
+	function onKeydownEventHandler(keyboardEvent){
+		if([32, 33, 34, 35, 36, 37, 38, 39, 40].indexOf(keyboardEvent.keyCode) > -1) {
+			keyboardEvent.preventDefault();
+		}
+	}
+
+	function onMouseEventHandler(mouseEvent){
+		if(mouseEvent.button==1)
+			mouseEvent.preventDefault();
+	}
 })
